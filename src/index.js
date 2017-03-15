0e730d95797ce0e2774576fa97f5f5998072c2ed
Håkon Rossebø <hakonrossebo@hotmail.com>
2017-03-15 14:01:04 +0100

    Changed to fullscreen

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 require('./main.css');
 var Elm = require('./Main.elm');
 
-var babelFishApp = Elm.Main.embed(document.getElementById('root'), {
-	currentTime: Date.now()
-});
+var babelFishApp = Elm.Main.fullscreen({ currentTime: Date.now() });
 
 babelFishApp.ports.scrollIdIntoView.subscribe(function(domId) {
 	 document.getElementById(domId).scrollIntoView();
