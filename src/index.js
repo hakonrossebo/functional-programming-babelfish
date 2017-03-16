a01196e248d0a1e02c83264b4100abb18a13848d
Håkon Rossebø <hakonrossebo@hotmail.com>
2017-03-16 08:54:12 +0100

    Trying to fix highlight, fixing html

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
+require('file?name=/assets/highlight/highlight.pack.js!../assets/highlight/highlight.pack.js');
 require('./main.css');
 require('file?name=available-languages.json!../available-languages.json');
 require('file?name=concepts.json!../concepts.json');
 require('file?name=/images/elm.png!../images/elm.png');
 require('file?name=/assets/highlight/solarized-dark.css!../assets/highlight/solarized-dark.css');
-require('file?name=/assets/highlight/highlight.pack.js!../assets/highlight/highlight.pack.js');
 require('file?name=/assets/analytics.js!../assets/analytics.js');
 
 var Elm = require('./Main.elm');
 
-var babelFishApp = Elm.Main.fullscreen({ currentTime: Date.now() });
+var babelFishApp = Elm.Main.embed(document.getElementById('root'), {
+	currentTime: Date.now()
+});
+// var babelFishApp = Elm.Main.fullscreen({ currentTime: Date.now() });
 
 babelFishApp.ports.scrollIdIntoView.subscribe(function(domId) {
 	 document.getElementById(domId).scrollIntoView();
